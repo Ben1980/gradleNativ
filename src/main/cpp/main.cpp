257e482b40ba2de24448c87d1717ebd8a492bf79
Benjamin Mahr <ben.mahr@gmail.com>
2018-07-12 15:20:49 +0200

    greeter library added

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,7 +1,10 @@
-#include <iostream>   
+#include "greeter.hpp"
+#include <iostream>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+int main(int argc, char* argv[]) {
+    Greeter g("Gradle User");
+    g.greet();
+	
 	std::cin;
     return 0;
 }
