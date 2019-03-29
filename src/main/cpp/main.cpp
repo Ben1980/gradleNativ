8fe697ebac7e0f813338f91380e2ed9cc55a5b6f
Benjamin Mahr <ben.mahr@gmail.com>
2019-03-29 15:31:51 +0100

    Build works with simple main file

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,10 +1,12 @@
-#include "greeter.hpp"
+//#include "greeter.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    Greeter g("Gradle User");
-    g.greet();
+	std::cout << "Hello World!" << std::endl;
 	
-	std::cin;
+	//Greeter g("Gradle User");
+    //g.greet();
+	
+	//std::cin;
     return 0;
 }
